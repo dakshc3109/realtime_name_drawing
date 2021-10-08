d0928ca1dbd3c379bcf9ef16617c4b4782e40f49
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-10-08 14:47:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 //main.js
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(500, 400);
+    canvas.position(600, 100);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function draw(){
+    canvas.background("#FF0000");
+}
+
+function modelLoaded(){
+    console.log("Posenet is Loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
