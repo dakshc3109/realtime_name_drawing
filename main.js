c6c75331fb4fd3b48be0f46eceb40f770a4376b9
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-10-09 12:09:40 +0530

    completed project

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-//main.js
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(500, 500);
@@ -9,17 +13,25 @@ function setup(){
     posenet = ml5.poseNet(video, modelLoaded);
     posenet.on('pose', gotPoses);
 }
-
 function draw(){
+    document.getElementById("name").innerHTML = "The side of square is "+difference+" px";
     canvas.background("#FF0000");
+    fill("#ecfc03");
+    stroke("#ecfc03");
+    square(noseX, noseY, difference);
 }
-
 function modelLoaded(){
-    console.log("Posenet is Loaded");
+    console.log("Pose net is Loaded");
 }
-
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX- "+noseX+" noseY- "+noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("left Wrist x- "+leftWristX+" right wrist x- "+rightWristX+" Difference- "+difference);
     }
 }
